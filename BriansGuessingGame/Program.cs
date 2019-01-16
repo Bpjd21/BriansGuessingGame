2bba993dab8cf6d3a2a275fd6baf4c8e54a9cc4d
Brian <bpjd21@yahoo.com>
2019-01-16 11:33:11 -0500

    chg goodbye to adios

--- BriansGuessingGame/Program.cs
+++ BriansGuessingGame/Program.cs
@@ -7,7 +7,7 @@ namespace BriansGuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Adios");
             Console.ReadKey();
         }
     }
