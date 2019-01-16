9526cc73173ebad3e1fdf00c3dd58b802ea71e0f
Brian <bpjd21@yahoo.com>
2019-01-16 11:12:06 -0500

    first commit

---
+++ BriansGuessingGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BriansGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
