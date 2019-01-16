463926717536726f7a730eac8401afa683f5f5b6
Brian <bpjd21@yahoo.com>
2019-01-16 14:12:26 -0500

    added help options

--- BriansGuessingGame/Program.cs
+++ BriansGuessingGame/Program.cs
@@ -6,20 +6,30 @@ namespace BriansGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please guess a number between 1 and 10.");
-            string userGuess = Console.ReadLine();
-            Console.WriteLine("You Guessed: " + userGuess);
 
-           
+            Console.WriteLine("Please guess a number between 1 and 10 or enter 0 for help");
+            string userGuess = Console.ReadLine();
+
+            if (userGuess.Equals("0"))
+            {
+                Console.WriteLine("Game Instructions are:");
+                Console.WriteLine("Guess a Number betwwen 1 and 10");
+                Console.WriteLine("Hit Enter to submit your guess.");
+
+                userGuess = Console.ReadLine();
+            }
+
+            Console.WriteLine("You guessed: " + userGuess);
+
             if (userGuess.Equals("7"))
-            {               
-                Console.WriteLine("You Win!");
+            {
+                Console.WriteLine("You Win");
             }
             else
-            {            
+            {
                 Console.WriteLine("You Lose");
             }
-            
+                                           
 
         }
     }
